5c6c1ea6e0f65ad6b3184fd4c5ced95f75292e48
Samuel Augusto De Lima Souza EXTERNO <10402963@edp.com.br>
2024-11-20 17:24:51 -0300

    implementado dependencias do projeto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
-﻿using DesafioPOO.Models;
+﻿// TODO: Realizar os testes com as classes Nokia e Iphone
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", IMEI: "111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+Console.WriteLine("Smartphone Iphone:");
+
+Smartphone iphone = new Nokia(numero: "654321", modelo: "Modelo 2", IMEI: "222222", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
